7130c7249145a1e0602023af53d9a353327ed4cc
Vlad <vladislav94@gmail.com>
2016-12-23 23:01:26 +0200

    connection to db works

--- dw.js
+++ dw.js
@@ -12,44 +12,44 @@ var url = 'mongodb://localhost:27017/mydb';
 var str = "";
 
 app.route('/persons').get(function(req,res){
-  mongoClient.connect(url, function(err, db){
-    var cursor = db.collection('persons').find({});
-      cursor.each(function(err,item){
-          if(item != null){
-            str = str + "&nbsp;&nbsp;&nbsp;&nbsp;" + item.firstname + "</br>";
-          }
-      });
-      res.send(str);
-      //var cursor = collection.find({});
-  });
+    mongoClient.connect(url, function(err, db){
+        var cursor = db.collection('persons').find({});
+        cursor.each(function(err,item){
+            if(item != null){
+                str = str + "&nbsp;&nbsp;&nbsp;&nbsp;" + item.firstname + "</br>";
+            }
+        });
+        res.send(str);
+        //var cursor = collection.find({});
+    });
 
 
 });
 
 var server = app.listen(8081,function(){});
 /*
-// Use connect method to connect to the Server
-mongoClient.connect(url, function (err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-        console.log('Connection established to', url);
-// Get the documents collection
-        var collection = db.collection('persons');
-        collection.find({}).toArray(function (err, result) {
-            if (err) {
-                console.log(err);
-            } else if (result.length) {
-                console.log('Found:', JSON.stringify(result));
-            } else {
-                console.log('No document(s) found with defined "find" criteria!');
-            }
-            //Close connection
-            //db.close();
-        });
-        // do some work here with the database.
+ // Use connect method to connect to the Server
+ mongoClient.connect(url, function (err, db) {
+ if (err) {
+ console.log('Unable to connect to the mongoDB server. Error:', err);
+ } else {
+ console.log('Connection established to', url);
+ // Get the documents collection
+ var collection = db.collection('persons');
+ collection.find({}).toArray(function (err, result) {
+ if (err) {
+ console.log(err);
+ } else if (result.length) {
+ console.log('Found:', JSON.stringify(result));
+ } else {
+ console.log('No document(s) found with defined "find" criteria!');
+ }
+ //Close connection
+ //db.close();
+ });
+ // do some work here with the database.
 
-        //Close connection
-        //db.close();
-    }
-});*/
+ //Close connection
+ //db.close();
+ }
+ });*/
