8c62e3f9cb4857a753f0cf8ee73108b5582fb251
Vlad <vladislav94@gmail.com>
2016-12-23 23:57:18 +0200

    add new routes

--- dw.js
+++ dw.js
@@ -13,47 +13,42 @@ var url = 'mongodb://localhost:27017/mydb';
 
 var str = "";
 
-app.route('/persons').get(function(req,res){
+//http://localhost:8081/
+app.route('/').get(function(req,res){
+   res.send("Datawarehouse");
+});
+
+//http://localhost:8081/personslist
+app.route('/personslist').get(function(req,res){
     mongoClient.connect(url, function(err, db){
         db.collection('persons').find().toArray(function(err, result){
            if(err){
                throw err;
            }
-           res.send(result);
+           res.json(result);
         });
     });
 
 });
 
+//http://localhost:8081/persons?sex=F&group=FI-131
+app.route('/persons').get(function(req,res){
+   mongoClient.connect(url,function(err,db){
+       var query = require('url').parse(req.url,true).query;
+      db.collection('persons').find({sex:query.sex, group:query.group}, {}).toArray(function(err,result){
+          if(err){
+              throw err;
+          }
+          res.send(result);
+      });
+   });
+});
+
+
 var server = app.listen(PORT,function(){
     console.log("DataWareHouse listening on: http://localhost:%s", PORT);
 });
-/*
- // Use connect method to connect to the Server
- mongoClient.connect(url, function (err, db) {
- if (err) {
- console.log('Unable to connect to the mongoDB server. Error:', err);
- } else {
- console.log('Connection established to', url);
- // Get the documents collection
- var collection = db.collection('persons');
- collection.find({}).toArray(function (err, result) {
- if (err) {
- console.log(err);
- } else if (result.length) {
- console.log('Found:', JSON.stringify(result));
- } else {
- console.log('No document(s) found with defined "find" criteria!');
- }
- //Close connection
- //db.close();
- });
- // do some work here with the database.
 
- //Close connection
- //db.close();
- }
- });*/
 /*
 //Lets require/import the HTTP module
 var http = require('http');
