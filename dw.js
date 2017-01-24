0192f7418745d88abee8fd6fb25e46cde034732b
quazzzar <abashuiasa@gmail.com>
2017-01-24 23:34:58 +0200

    insert from form, html restyle

--- dw.js
+++ dw.js
@@ -1,4 +1,3 @@
-
 //lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
 var express = require('express');
@@ -14,13 +13,13 @@ var url = 'mongodb://localhost:27017/mydb';
 //http://localhost:8081/
 app.use(function(request, response, next) {
 
-    console.time('handler name');
-    // log each request to the console
-    console.log(request.method, request.url);
-    // continue doing what we were doing and go to the route
-    next();
-    console.timeEnd('handler name');
-    console.log("--------------------------------")
+	console.time('handler name');
+	// log each request to the console
+	console.log(request.method, request.url);
+	// continue doing what we were doing and go to the route
+	next();
+	console.timeEnd('handler name');
+	console.log("--------------------------------")
 });
 
 app.route('/').get(function(request,response){
@@ -30,93 +29,93 @@ app.route('/').get(function(request,response){
 // http://localhost:8081/persons
 // http://localhost:8081/persons?sex=F&group=FI-131
 app.route('/persons').get(function(request,response){
-    console.log(request.method);// pune acolo, dai run
-    mongoClient.connect(url, function(err, db){
-        var query = require('url').parse(request.url,true).query;
-        if(query.group != null && query.sex !=null ){
-            var query = require('url').parse(request.url,true).query;
-            db.collection('persons').find({sex:query.sex, group:query.group}, {}).toArray(function(err,result){
-                if(err){
-                    throw err;
-                } else{
-                    response.json(result);
-                }
-            });
-        } else {
-            db.collection('persons').find().toArray(function(err, result){
-                if(err){
-                    throw err;
-                }
-                response.json(result);
-            });
-        }
-    });
+	console.log(request.method);// pune acolo, dai run
+	mongoClient.connect(url, function(err, db){
+		var query = require('url').parse(request.url,true).query;
+		if(query.group != null && query.sex !=null ){
+			var query = require('url').parse(request.url,true).query;
+			db.collection('persons').find({sex:query.sex, group:query.group}, {}).toArray(function(err,result){
+				if(err){
+					throw err;
+				} else{
+					response.json(result);
+				}
+			});
+		} else {
+			db.collection('persons').find().toArray(function(err, result){
+				if(err){
+					throw err;
+				}
+				response.json(result);
+			});
+		}
+	});
 
 });
 
 // http://localhost:8081/persons/Balan  -search by firstname
 app.route("/persons/:firstname").get(function(request, response) {
-    mongoClient.connect(url, function (err, db) {
-        db.collection('persons').find({'firstname':request.params.firstname}, {}).toArray(function (err, result) {
-            if (err) {
-                throw err;
-            }
-            response.json(result);
-        });
-    });
+	mongoClient.connect(url, function (err, db) {
+		db.collection('persons').find({'firstname':request.params.firstname}, {}).toArray(function (err, result) {
+			if (err) {
+				throw err;
+			}
+			response.json(result);
+		});
+	});
 });
 
 //not work yet
 app.route('/deleteperson/:id').delete(function(request,response){
-    mongoClient.connect(url, function(err,db){
-        var personToDelete = request.params.id;
-        db.collection('persons').remove({ '_id':personToDelete}, function(err){
-            response.send((err === null) ? {msg: ''} : {msg:'error: '+err});
-        });
-    });
+	mongoClient.connect(url, function(err,db){
+		var personToDelete = request.params.id;
+		db.collection('persons').remove({ '_id':personToDelete}, function(err){
+			response.send((err === null) ? {msg: ''} : {msg:'error: '+err});
+		});
+	});
 });
 
 //it works with post method
 app.route('/insert').post(function (request, response) {
-    mongoClient.connect(url,function(err,db){
-        //Exemplu pentru a scoate date din body
-        // Get our form values. These rely on the "name" attributes
-        //var userName = request.body.username;
-        //var userEmail = request.body.useremail;
-        /*var firstname = "Ciocan"; //eu am pus direct string //proxy o sa trimita date
-        var lastname = "Ion";
-        var age = 21;
-        var sex = "M";
-        var group = "FI-121";
-        // Submit to the DB*/
-        db.collection('persons').insert({
-            "firstname" : firstname,
-            "lastname" : lastname,
-            "age": age,
-            "sex": sex,
-            "group": group
-        }, function (err, doc) {
-            if (err) {
-                // If it failed, return error
-                response.send("There was a problem adding the information to the database.");
-            }
-            else {
-                // And forward to persons page
-                response.redirect("persons");
-            }
-        });
+	mongoClient.connect(url,function(err,db){
+		//Exemplu pentru a scoate date din body
+		// Get our form values. These rely on the "name" attributes
+		//var userName = request.body.username;
+		//var userEmail = request.body.useremail;
+		var firstname = request.query.firstname; //eu am pus direct string //proxy o sa trimita date
+		var lastname = request.query.lastname;
+		var age = parseInt(request.query.age, 10);
+		var sex = request.query.sex;
+		var group = request.query.group;
+		// Submit to the DB
+		db.collection('persons').insert({
+			"firstname" : firstname,
+			"lastname" : lastname,
+			"age": age,
+			"sex": sex,
+			"group": group
+		}, function (err, doc) {
+			if (err) {
+				// If it failed, return error
+				response.send("There was a problem adding the information to the database.");
+			}
+			else {
+				// And forward to persons page
+				response.redirect("persons");
+			}
+		});
 
-    });
+	});
 });
 
 /*
 app.post('/addUser', function(req, res){
-    console.dir(req.body);
-    res.send("test");
+	console.dir(req.body);
+	res.send("test");
 });
 */
 
 var server = app.listen(PORT,function(){
-    console.log("DataWareHouse listening on: http://localhost:%s", PORT);
+	console.log("DataWareHouse listening on: http://localhost:%s", PORT);
 });
 
--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-//var parser = require("xml2json");
 var jsonxml = require("jsontoxml");
 var request = require('request');
 var express = require('express');
@@ -10,94 +9,61 @@ var fs = require('fs');
 
 
 app.listen(9000, function () {
-   console.log("proxy started on 9000!!!");
+	 console.log("proxy started on 9000!!!");
 });
 
 app.get('/', function (req, res) {
 
-    fs.readFile('./html/clientPage.html', function (err, html) {
-        if (err) {
-            throw err;
-        }
-        res.writeHeader(200, {"Content-Type": "text/html"});
-        res.write(html);
-        res.end();
-    });
+		fs.readFile('./html/clientPage.html', function (err, html) {
+				if (err) {
+						throw err;
+				}
+				res.writeHeader(200, {"Content-Type": "text/html"});
+				res.write(html);
+				res.end();
+		});
 });
 
 app.get("/insert", function (req, res) {
+	res.writeHead(301,  {Location: '/'} );
+	res.end();
 
-    person = {
-        firstname:req.query.firstname,
-        lastname:req.query.lastname,
-        age:req.query.age,
-        group:req.query.group,
-        sex:req.query.sex
-    };
-    console.log(person);
-    res.send(person);
+	request.post({
+		headers: {'content-type' : 'application/json'},
+		url:     'http://localhost:8081/insert?firstname='+ req.query.firstname +'&lastname=' + req.query.lastname + '&age=' + req.query.age + '&group=' + req.query.group + '&sex=' + req.query.sex
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			 // send back to client
+				
+		}
+	});
 
-    request.post("http://localhost:8081/persons", function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-           // send back to client
-            res.write();
-            res.end();
-        }
-    });
+	setTimeout(function(){ 
+		alert("Inserted !"); }, 1000);
 
-
-   // console.log(req.params("firsname"));
-   //  res.writeHeader(200, {"Content-Type": "text/html"});
-   //  //res.write(req.firstname);
-   //
-   //  var querry = require("url").parse(req.url, true).query;
-   //  var name = querry.firstname;
-   //  res.write(name);
-   //  res.end();
-    //req.send(res);
 });
 
 app.get("/persons", function (req, res) {
+	console.log(req.url.toString());
 
-    // client.set('string key', 'Hello World', redis.print);
-// // Expire in 3 seconds
-// client.expire('string key', 3);
-// // get from
-// client.get('string key');
-
-   // var query = require('url').parse(req.url,true);
-    console.log(req.url.toString());
-
-    if (redisClient.get(req.url) == null ) {
-        // se afla in cache
-
-        res.write(" " + redisClient.get(req.url)); //aici da true
-        res.end();
-
-    } else {
-        // nu se gaseste in cache
-        request("http://localhost:8081/persons", function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-
-                // put in cache
-                redisClient.set(req.url, body, redis.print);
-                var abc = jsonxml(body);
-                res.write('<html><head></head><body>');
-                res.write('<p>Respose here: </p>');
-                //res.write('<p><textarea for="responseTextarea" rows="15" cols="80">'+ parser.toXml(body, options) +'</textarea></p>');
-               // res.write('<p><textarea for="responseTextarea" rows="25" cols="120">'+ jsonxml(body) +'</textarea></p>');
-               res.write('<p><textarea for="responseTextarea" rows="25" cols="120">'+ jsonxml(body) +'</textarea></p>');
-                res.end('</body></html>');
-
-                
-
-
-
-            }
-        });
-    }
-
-
-
+	if (redisClient.get(req.url) == null ) {
+		// se afla in cache
+		res.write(" " + redisClient.get(req.url)); //aici da true
+		res.end();
+	} else {
+		// nu se gaseste in cache
+		request("http://localhost:8081/persons", function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				// put in cache
+				redisClient.set(req.url, body, redis.print);
+				var abc = jsonxml(body);
+				res.write('<html><head></head><body>');
+				res.write('<p>Respose here: </p>');
+				//res.write('<p><textarea for="responseTextarea" rows="15" cols="80">'+ parser.toXml(body, options) +'</textarea></p>');
+			 	// res.write('<p><textarea for="responseTextarea" rows="25" cols="120">'+ jsonxml(body) +'</textarea></p>');
+				res.write('<p><textarea for="responseTextarea" rows="25" cols="120">'+ jsonxml(body) +'</textarea></p>');
+				res.end('</body></html>');
+			}
+		});
+	}
 });
-
