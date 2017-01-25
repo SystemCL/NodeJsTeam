4d6b1c1dd16e3baeb60f92b6392f01a41ea62138
Vlad <vladislav94@gmail.com>
2017-01-25 21:49:45 +0200

    runWindows.bat file deleted

--- runWindows.bat
+++
@@ -1,6 +0,0 @@
-start D:\MongoDB\Server\3.4\bin\mongod.exe --dbpath E:\univer_FI-131\Anul_IV\PAD\pad-final1\data
-start D:\MongoDB\Server\3.4\bin\mongo.exe
-start cmd.exe /k "C:\Program Files\Redis\redis-cli.exe"
-start "dw1" /d "E:\univer_FI-131\Anul_IV\PAD\pad-final1" node dw.js 9500
-start "dw2" /d "E:\univer_FI-131\Anul_IV\PAD\pad-final1" node dw.js 9501
-start /d "E:\univer_FI-131\Anul_IV\PAD\pad-final1" node proxy.js
